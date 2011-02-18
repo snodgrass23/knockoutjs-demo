14f6e0a10bace8ea8e857a690299d70f89f93243
Jim Snodgrass <jim@skookum.com>
2011-02-18 08:20:33 -0500

    add additional functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var itemViewModel = function(options) {
     this.item = ko.observable(options.item || '');
+    this.hours = ko.observable(options.hours || '');
     this.isDone = function() { 
       //check if item is done  
       return false
@@ -16,13 +17,13 @@ itemViewModel.prototype = {
 
 var viewModel = {};
 viewModel.items = ko.observableArray([]);
-viewModel.itemToAdd = ko.observable("");
 viewModel.addItem = function () {
    // add item code
+   viewModel.items.push(new itemViewModel({item: $("#new-todo").val(), hours: 100}))
 }
 
-viewModel.items.push(new itemViewModel({item: "Do this"}))
-viewModel.items.push(new itemViewModel({item: "Do that"}))
+viewModel.items.push(new itemViewModel({item: "Do this", hours: 3}))
+viewModel.items.push(new itemViewModel({item: "Do that", hours: 4}))
 
 ko.applyBindings(viewModel);
 
